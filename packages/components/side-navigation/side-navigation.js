375af7c9b10160a54efacacef389adfd0b8b1edb
Borislav Napoleonov <borislav.napoleonov@wales.nhs.uk>
2021-08-12 07:55:08 +0100

    update sidebar style

--- packages/components/side-navigation/side-navigation.js
+++ packages/components/side-navigation/side-navigation.js
@@ -1,42 +1,47 @@
 export default () => {
   // grab the sections (targets) and menuLinks (triggers)
   // for menu items to apply active link styles to
+  const menu = document.querySelector('.nhsuk-side-navigation-options');
+  const spacer = document.querySelector('.nhsuk-side-navigation-spacer');
+  const header = document.querySelector('.nhsuk-header__container');
   const sections = document.querySelectorAll('.nhsuk-side-navigation__section');
   const menuLinks = document.querySelectorAll('.nhsuk-side-navigation__item a');
-
   // functions to add and remove the active class from links as appropriate
-  const makeActive = (link) => menuLinks[link].classList.add('nhsuk-side-navigation__current');
+  const makeActive = (link) => { if (link !== undefined) menuLinks[link].classList.add('nhsuk-side-navigation__current'); };
   const removeActive = (link) => menuLinks[link].classList.remove('nhsuk-side-navigation__current');
   const removeAllActive = () => [...Array(sections.length).keys()]
     .forEach((link) => removeActive(link));
-  const isNotActive = (link) => !menuLinks[link].classList.contains('nhsuk-side-navigation__current');
+  const makeFixed = (dom) => dom.classList.add('nhsuk-side-navigation-fixed');
+  const removeFixed = (dom) => dom.classList.remove('nhsuk-side-navigation-fixed');
   // change the active link a bit above the actual section
   // this way it will change as you're approaching the section rather
   // than waiting until the section has passed the top of the screen
   const sectionMargin = 100;
 
-  // keep track of the currently active link
-  // use this so as not to change the active link over and over
-  // as the user scrolls but rather only change when it becomes
-  // necessary because the user is in a new section of the page
-  let currentActive = 0;
+  function drawSidebar() {
+    if (window.scrollY - 20 >= (header.offsetTop + 1)) {
+      makeFixed(menu);
+      removeFixed(spacer);
+    } else {
+      removeFixed(menu);
+      makeFixed(spacer);
+    }
+  }
 
   function draw() {
     const current = sections.length - [...sections].reverse()
       .findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1;
 
+    drawSidebar();
+
     // only if the section has changed
     // remove active class from all menu links
     // and then apply it to the link for the current section
-    if ((current !== currentActive) || isNotActive(current)) {
-      removeAllActive();
-      currentActive = current;
-      makeActive(current);
-    }
+    removeAllActive();
+    makeActive(current);
   }
 
-  // Draw once on page load.
-  draw();
+  drawSidebar();
 
   // listen for scroll events
   window.addEventListener('scroll', () => draw());
